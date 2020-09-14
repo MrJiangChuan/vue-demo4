1aeefcebc157251fdfaaec92c22e63e4252652b3
jiangchuan <jiangchuan_1994@163.com>
2020-09-15 02:10:07 +0800

    Promise基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <input type="text" disabled="true" v-model="msg" />
+      <input type="button" value="发送请求" @click="handler" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data: () => {
+	  return {
+      msg: '冲冲冲'
+    }
+  },
+  methods: {
+    handler: () => {
+      let p = new Promise((resolve, reject) => {
+        let flag = Math.random()//[0,1)
+        setTimeout(() => {
+          if(flag>=0.5){
+            resolve('请求成功')
+          }else{
+            reject('请求失败')
+          }
+        },2000)
+      })
+
+      p.then((ret) => {
+        console.log(ret)
+
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
