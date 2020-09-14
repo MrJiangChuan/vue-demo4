4c2bf9ed5de54d0c5c58d89317c9fde5aa2161f4
jiangchuan <jiangchuan_1994@163.com>
2020-09-15 04:22:55 +0800

    接口调用fetch用法

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
       <hr>
       <h3>2. 基于Promise处理Ajax请求</h3>
       <input type="button" value="发送Ajax请求" @click="handler1" />
+      <hr>
+      <h3>3. 接口调用fetch用法</h3>
+      <input type="button" value="发送fetch请求[Get]" @click="handler2" />
+      <input type="button" value="发送fetch请求[POST/参数]" @click="handler3" />
+      <input type="button" value="发送fetch请求[POST/JSON]" @click="handler4" />
   </div>
 </template>
 
@@ -60,8 +65,64 @@ export default {
       this.queryData('http://localhost:8080/api/user/info/1')
       .then((data) => {
         console.log(data)
+        return this.queryData('http://localhost:8080/api/user/info/2')
       },(error) => {
         console.log(error)
+      })
+      .then((data) => {
+        console.log(data)
+         return this.queryData('http://localhost:8080/api/user/info/3')
+      },(error) => {
+        console.log(error)
+      })
+      .then((data) => {
+        console.log(data)
+      },(error) =>{
+        console.log(error)
+      })
+    },
+    handler2() {
+      fetch('http://localhost:8080/api/user/info/4')
+      .then(data => {
+        return data.text()
+      })
+      .then(ret => {
+        console.log(ret)
+      })
+    },
+    handler3() {
+      fetch('http://localhost:8080/api/user/add',{
+      method: 'POST',
+      body: 'id=1&name=jerry&sex=男&age=22',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }})
+      .then(data => {
+        return data.text()
+      })
+      .then(ret => {
+        console.log(ret)
+      })
+    },
+    handler4() {
+      fetch('http://localhost:8080/api/user/add',{
+      method: 'post',
+      body: JSON.stringify({
+        id: 2,
+        name: 'Tom',
+        sex: '女',
+        age: 28
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }})
+      .then(data => {
+        // return data.text()
+        return data.json()
+      })
+      .then(ret => {
+        console.log(ret)
+        console.log(ret.id+'--'+ret.name+'--'+ret.sex+'--'+ret.age)
       })
     }
   },
